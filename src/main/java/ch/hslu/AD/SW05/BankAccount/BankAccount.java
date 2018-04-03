3fbe1064ce12c48f334cbabe4997f39701a2f9de
Fabian Gröger <fabian.groeger@bluewin.ch>
2018-04-03 22:58:55 +0200

    13. März 2018, SW05 - Übung: Threads & Synchronisation (N1), EX01 überarbeitet

--- src/main/java/ch/hslu/AD/SW05/BankAccount/BankAccount.java
+++ src/main/java/ch/hslu/AD/SW05/BankAccount/BankAccount.java
@@ -11,27 +11,56 @@ package ch.hslu.AD.SW05.BankAccount;
  */
 public final class BankAccount {
 
-    private int balance;
-    private String name;
+    private int balance; // balance of the bank account
+    private String name; // name of the bank account
 
+    /**
+     * Default constructor, that creates a bank account with balance: 0 and name: default
+     */
+    public BankAccount() {
+        this(0, "default");
+    }
 
+    /**
+     * Constructor that creates a new bank account with the passed balance and name
+     *
+     * @param balance balance of the new bank account
+     * @param name    name of the new bank account
+     */
     public BankAccount(final int balance, final String name) {
         this.balance = balance;
         this.name = name;
     }
 
+    /**
+     * Returns the balance of the bank account
+     *
+     * @return balance of the account
+     */
     public int getBalance() {
         return this.balance;
     }
 
+    /**
+     * Deposits an amount in the bank account
+     *
+     * @param amount amount to add to the bank account
+     */
     public synchronized void deposite(final int amount) {
         this.balance += amount;
     }
 
+    /**
+     * Transfers an amount from the bank account to the target bank account
+     *
+     * @param target the target bank account that will get the amount
+     * @param amount the amount that is transferred
+     */
     public void transfer(final BankAccount target, final int amount) {
         synchronized (this) {
             this.balance -= amount;
         }
+
         target.deposite(amount);
     }
 
@@ -49,7 +78,8 @@ public final class BankAccount {
      *
      * @return string representation of the bank account
      */
+    @Override
     public String toString() {
-        return name + " (" + String.valueOf(balance) + ".-)";
+        return "BankAccount [name: " + this.name + " balance: " + this.balance + "]";
     }
 }
