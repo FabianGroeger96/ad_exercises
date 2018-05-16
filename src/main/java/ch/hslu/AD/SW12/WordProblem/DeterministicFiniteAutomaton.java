15ecd3d7fde54608dd5078b8354cf81f0c8fb133
Fabian Gröger <fabian.groeger@bluewin.ch>
2018-05-16 11:26:07 +0200

    13. März 2018, SW12 - Übung: Endliche Automaten und formale Sprachen (A3), EX07 edited

--- src/main/java/ch/hslu/AD/SW12/WordProblem/DeterministicFiniteAutomaton.java
+++ src/main/java/ch/hslu/AD/SW12/WordProblem/DeterministicFiniteAutomaton.java
@@ -10,17 +10,17 @@ package ch.hslu.AD.SW12.WordProblem;
 public class DeterministicFiniteAutomaton {
 
     public static boolean isWordLanguage(final String string) {
-        String state = "z0"; // initial state
+        State state = State.z0; // initial state
 
         for (char c : string.toCharArray()) {
             switch (c) {
                 case '0':
                     switch (state) {
-                        case "z0":
-                            state = "z1"; // if 0 & is at z0 -> z1
+                        case z0:
+                            state = State.z1; // if 0 & is at z0 -> z1
                             break;
-                        case "z2":
-                            state = "z4"; // if 0 & is at z2 -> z4
+                        case z2:
+                            state = State.z4; // if 0 & is at z2 -> z4
                             break;
                         default:
                             return false;
@@ -29,17 +29,17 @@ public class DeterministicFiniteAutomaton {
 
                 case '1':
                     switch (state) {
-                        case "z1":
-                            state = "z2"; // if 1 & is at z1 -> z2
+                        case z1:
+                            state = State.z2; // if 1 & is at z1 -> z2
                             break;
-                        case "z2":
-                            state = "z3"; // if 1 & is at z2 -> z3
+                        case z2:
+                            state = State.z3; // if 1 & is at z2 -> z3
                             break;
-                        case "z3":
-                            state = "z2"; // if 1 & is at z3 -> z2
+                        case z3:
+                            state = State.z2; // if 1 & is at z3 -> z2
                             break;
-                        case "z4":
-                            state = "z2"; // if 1 & is at z4 -> z2
+                        case z4:
+                            state = State.z2; // if 1 & is at z4 -> z2
                             break;
                         default:
                             return false;
@@ -51,6 +51,6 @@ public class DeterministicFiniteAutomaton {
             }
         }
 
-        return "z4".equals(state) || "z1".equals(state); // z4 & z1 -> terminal symbols
+        return State.z4.equals(state) || State.z1.equals(state); // z4 & z1 -> terminal symbols
     }
 }
---
+++ src/main/java/ch/hslu/AD/SW12/WordProblem/State.java
@@ -0,0 +1,12 @@
+package ch.hslu.AD.SW12.WordProblem;
+
+/**
+ * Übung: Endliche Automaten und formale Sprachen (A3)
+ * Aufgabe: Wortproblem mit Hilfe eine DEA lösen
+ *
+ * @author Fabian Gröger
+ * @version 16.05.2018
+ */
+public enum State {
+    z0, z1, z2, z3, z4
+}
